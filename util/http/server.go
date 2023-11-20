75c6afba05f14738a150a3676c02dda07306fb11
James <32513142+jamesryancoleman@users.noreply.github.com>
2023-11-20 11:54:46 -0500

    added container trigger from root url

--- util/http/server.go
+++ util/http/server.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
 )
 
 func ReadBody(req *http.Request) []byte {
@@ -27,15 +34,16 @@ func HandleDefaultEndpoint(w http.ResponseWriter, req *http.Request) {
 	// for testing just print the body.
 	body := ReadBody(req)
 	log.Printf("\"/\" root endpoint called with method %s:\n%s", req.Method, string(body))
+	RunContainer("debian:bookwork-slim")
 }
 
-func main() {
-	// ctx := context.Background()
-	// cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer cli.Close()
+func RunContainer(img string) {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	defer cli.Close()
 
 	// // pull image from remote repo
 	// reader, err := cli.ImagePull(ctx, "docker.io/library/debian:bookworm-slim", types.ImagePullOptions{})
@@ -46,35 +54,37 @@ func main() {
 	// defer reader.Close()
 	// io.Copy(os.Stdout, reader)
 
-	// resp, err := cli.ContainerCreate(ctx, &container.Config{
-	// 	Image: "debian:bookworm-slim",
-	// 	Cmd:   []string{"echo", "hello world"},
-	// 	Tty:   false,
-	// }, nil, nil, nil, "")
-	// if err != nil {
-	// 	panic(err)
-	// }
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: img,
+		Cmd:   []string{"echo", "I was striggered by the webhook and default endpoint"},
+		Tty:   false,
+	}, nil, nil, nil, "")
+	if err != nil {
+		panic(err)
+	}
 
-	// if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-	// 	panic(err)
-	// }
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		panic(err)
+	}
 
-	// statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
-	// select {
-	// case err := <-errCh:
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// case <-statusCh:
-	// }
+	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			panic(err)
+		}
+	case <-statusCh:
+	}
 
-	// out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
-	// if err != nil {
-	// 	panic(err)
-	// }
+	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+	if err != nil {
+		panic(err)
+	}
 
-	// stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+}
 
+func main() {
 	// set logging flags
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
 
