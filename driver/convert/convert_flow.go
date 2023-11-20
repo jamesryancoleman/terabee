f8fde444c00f52f378bf833e1fdc55c7de46faac
James <32513142+jamesryancoleman@users.noreply.github.com>
2023-11-20 11:39:41 -0500

    mvp

---
+++ driver/convert/convert_flow.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+type FlowIn struct {
+	TS     int64       `json:"at"`
+	Serial string      `json:"serial_number"`
+	Flow   FlowInValue `json:"value"`
+}
+
+type FlowInValue struct {
+	In    int    `json:"in"`
+	Out   int    `json:"out"`
+	Reset string `json:"reset_period"`
+}
+
+type FlowFrost struct {
+	TS     string     `json:"phenomenonTime"`
+	RTS    string     `json:"resultTime"`
+	Result int        `json:"result"`
+	Stream Datastream `json:"Datastream"`
+}
+
+type Datastream struct {
+	Id int `json:"@iot.id"`
+}
+
+func (f FlowInValue) GetOccupancy() int {
+	return f.In - f.Out
+}
+
+func Flow2Frost(in FlowIn) FlowFrost {
+	out := FlowFrost{}
+
+	out.TS = time.Unix(in.TS, 0).Format(time.RFC3339)
+	out.RTS = out.TS
+	out.Result = in.Flow.GetOccupancy()
+	out.Stream.Id, _ = strconv.Atoi(os.Args[2])
+
+	return out
+}
+
+func main() {
+	// read the command line arg
+	msg := os.Args[1]
+
+	// unmarshall the raw msg
+	flowIn := FlowIn{}
+	json.Unmarshal([]byte(msg), &flowIn)
+
+	// convert to frost format
+	flowOut := Flow2Frost(flowIn)
+	b, _ := json.Marshal(flowOut)
+
+	fmt.Println(string(b))
+}
---
+++ driver/post/post.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
+func readStdin() string {
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+	return text
+}
+
+func main() {
+
+	client := &http.Client{}
+
+	req, _ := http.NewRequest("POST", os.Args[1], bytes.NewBuffer([]byte(readStdin())))
+	req.Header.Add("Authorization", "Basic "+basicAuth(os.Args[2], os.Args[3]))
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Response Status:", resp.Status)
+}
