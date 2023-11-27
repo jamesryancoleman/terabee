7e10379800619de59caa71c019b5de9a145946b0
James <32513142+jamesryancoleman@users.noreply.github.com>
2023-11-27 11:36:27 -0500

    added a simple server

---
+++ util/http-echo/server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func ReadBody(req *http.Request) []byte {
+	defer req.Body.Close()
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		panic("error parsing body of request")
+	}
+	return body
+}
+
+func HandleFlow(w http.ResponseWriter, req *http.Request) {
+	// for testing just print the body.
+	body := ReadBody(req)
+	log.Printf("\"/terabee/flow\" endpoint called with method %s:\n%s", req.Method, string(body))
+}
+
+// a default endpoint to confirm receipt of a http-post
+func HandleDefaultEndpoint(w http.ResponseWriter, req *http.Request) {
+	// for testing just print the body.
+	body := ReadBody(req)
+	log.Printf("\"/\" root endpoint called with method %s:\n%s", req.Method, string(body))
+}
+
+func main() {
+	// set logging flags
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+
+	// define handlers
+	http.HandleFunc("/terabee/flow", HandleFlow)
+	http.HandleFunc("/", HandleDefaultEndpoint)
+
+	// start the server
+	default_port := 8080
+	log.Println("== Starting HTTP Server ==")
+	log.Printf("== Listening on port %d ==", default_port)
+	http.ListenAndServe(fmt.Sprintf(":%d", default_port), nil)
+}
